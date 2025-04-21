bd8548d67282dd675d66f1cd0404b5fa87bfacd7
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-04-21 18:38:41 +0500

    status code printed to check its status

--- practice-componse/tests/test_home.py
+++ practice-componse/tests/test_home.py
@@ -5,4 +5,5 @@ import requests
 @pytest.fixture
 def test_home_page():
     response = requests.get("http://localhost:8080/")
+    print('::Status Code::', response.status_code == 200)
     return response.status_code == 200
